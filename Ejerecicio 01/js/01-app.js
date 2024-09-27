02c19554eb2a7d8f1e969463ce66b6bad17e4d6c
AndriuuU <andresitomolinaclan@gmail.com>
2024-09-27 10:49:20 +0200

    Primeras actividades hechas

---
+++ Ejerecicio 01/js/01-app.js
@@ -0,0 +1,11 @@
+
+let cadena = "Hola, mundo";
+let numero = 42;
+let booleano = true;
+
+console.log(typeof cadena);
+console.log(typeof numero);
+console.log(typeof booleano);
+
+let numeroComoCadena = String(numero);
+console.log(typeof numeroComoCadena);
---
+++ Ejerecicio 03/js/01-app.js
@@ -0,0 +1,23 @@
+let numero1 = 4;
+let numero2 = 6;
+
+let suma = numero1 + numero2;
+console.log("La suma es: " + suma);
+
+let resta = numero1 - numero2;
+console.log("La resta es: " + resta);
+
+let multiplicacion = numero1 * numero2;
+console.log("La multiplicación es: " + multiplicacion);
+
+let division = numero1 / numero2;
+console.log("La división es: " + division);
+
+let resto = numero1 % numero2;
+console.log("El resto es: " + resto);
+
+numero1++;
+console.log("Después del incremento, numero1 es: " + numero1);
+
+numero2--;
+console.log("Después del decremento, numero2 es: " + numero2);
